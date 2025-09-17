08adb755ddf31305ef541bc39c868f38964db597
souhaila <souhailalh77@gmail.com>
2025-09-17 16:03:00 +0100

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function addition(a, b) {
-    return a + b;
-  }
-  
-  let somme = addition(8, 5);
-  console.log("La somme est : " + somme);
+let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
