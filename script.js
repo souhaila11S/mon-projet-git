8342c4fba67796ee5787241158d7fd1e2d9cb12a
souhaila <souhailalh77@gmail.com>
2025-09-17 14:44:15 +0100

    Gitproject

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let nombre1 = 10;
 let nombre2 = 5;
 let somme = nombre1 + nombre2;
 console.log("La somme est : " + somme);
+console.log("La somme est : " + somme);console.log("La somme est : " + somme);
+console.log("La somme est : " + somme);
